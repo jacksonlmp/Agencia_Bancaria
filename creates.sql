8cc504d1868e33415c78d40ee4283bd21a1b1e4c
jacksonlmp <jacksonlmp17.jl@gmail.com>
2021-02-08 19:17:07 -0300

    referencia de nome_agencia

--- creates.sql
+++ creates.sql
@@ -1,20 +1,10 @@
-CREATE TABLE agencia(
-	nome_agencia varchar(50) NOT NULL,
-    cidade_agencia varchar(50) NOT NULL,
-    ativos int NOT NULL,
-    PRIMARY KEY(nome_agencia)
-);
-
-CREATE TABLE cliente(
-	nome_cliente varchar(50) NOT NULL,
-    endereco_cliente varchar(50) NOT NULL,
-    cidade_cliente varchar(50) NOT NULL,
-    PRIMARY KEY(nome_cliente)
-);
+use agencia_bancaria;
 
 CREATE TABLE emprestimo(
 	num_empre int NOT NULL,
     nome_agencia varchar(50) NOT NULL,
     valor int,
-    PRIMARY KEY(num_empre)
+    PRIMARY KEY(num_empre),
+    FOREIGN KEY(nome_agencia) REFERENCES agencia(nome_agencia)
 );
+
