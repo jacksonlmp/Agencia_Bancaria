b536aa6df926024cf8e234e9a8932af524202a80
jacksonlmp <jacksonlmp17.jl@gmail.com>
2021-02-08 19:18:09 -0300

    Referencias

--- creates.sql
+++ creates.sql
@@ -1,4 +1,16 @@
-use agencia_bancaria;
+CREATE TABLE agencia(
+	nome_agencia varchar(50) NOT NULL,
+    cidade_agencia varchar(50) NOT NULL,
+    ativos int NOT NULL,
+    PRIMARY KEY(nome_agencia)
+);
+
+CREATE TABLE cliente(
+	nome_cliente varchar(50) NOT NULL,
+    endereco_cliente varchar(50) NOT NULL,
+    cidade_cliente varchar(50) NOT NULL,
+    PRIMARY KEY(nome_cliente)
+);
 
 CREATE TABLE emprestimo(
 	num_empre int NOT NULL,
