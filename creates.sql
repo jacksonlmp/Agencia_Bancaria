b8b1118b7a503dcfa4168209ef47c75cccf34815
jacksonlmp <jacksonlmp17.jl@gmail.com>
2021-02-08 19:11:31 -0300

    Create de Agencia, CLiente e Emprestimo

---
+++ creates.sql
@@ -0,0 +1,20 @@
+CREATE TABLE agencia(
+	nome_agencia varchar(50) NOT NULL,
+    cidade_agencia varchar(50) NOT NULL,
+    ativos int NOT NULL,
+    PRIMARY KEY(nome_agencia)
+);
+
+CREATE TABLE cliente(
+	nome_cliente varchar(50) NOT NULL,
+    endereco_cliente varchar(50) NOT NULL,
+    cidade_cliente varchar(50) NOT NULL,
+    PRIMARY KEY(nome_cliente)
+);
+
+CREATE TABLE emprestimo(
+	num_empre int NOT NULL,
+    nome_agencia varchar(50) NOT NULL,
+    valor int,
+    PRIMARY KEY(num_empre)
+);
