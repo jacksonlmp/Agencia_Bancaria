af2976d1c7f2e2db74dd0cd9604dc7d165a8d348
itamarbernardo <itamarbernardo2013@gmail.com>
2021-02-08 20:08:59 -0300

    Add novas tabelas

--- creates.sql
+++ creates.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE agencia_bancaria;
+
 CREATE TABLE agencia(
 	nome_agencia varchar(50) NOT NULL,
     cidade_agencia varchar(50) NOT NULL,
@@ -20,3 +22,25 @@ CREATE TABLE emprestimo(
     FOREIGN KEY(nome_agencia) REFERENCES agencia(nome_agencia)
 );
 
+CREATE TABLE tomador(
+    nome_cliente varchar(50) NOT NULL,
+	num_empre int NOT NULL,
+    PRIMARY KEY(nome_cliente, num_empre),
+    FOREIGN KEY(nome_cliente) REFERENCES cliente(nome_cliente),
+    FOREIGN KEY(num_empre) REFERENCES emprestimo(num_empre)
+);
+
+CREATE TABLE conta(
+    num_conta int NOT NULL,
+    nome_agencia varchar(50) NOT NULL,
+	saldo double NOT NULL,
+    PRIMARY KEY(num_conta),
+    FOREIGN KEY(nome_agencia) REFERENCES agencia(nome_agencia)
+);
+
+CREATE TABLE depositante(
+    nome_cliente varchar(50) NOT NULL,
+	num_conta int NOT NULL,
+    PRIMARY KEY(nome_cliente, num_conta),
+    FOREIGN KEY(nome_cliente) REFERENCES cliente(nome_cliente),
+    FOREIGN KEY(num_conta) REFERENCES conta(num_conta)
